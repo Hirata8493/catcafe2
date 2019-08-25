7aed14300ffd25a0f4da29255058f719ec47f298
hirata <aki573378@gmail.com>
2019-08-25 15:25:29 +0900

    メニュー要素の追加

--- js/sample.js
+++ js/sample.js
@@ -21,6 +21,7 @@ $(function () {
     $('.nav-name').toggleClass('nav-name-2');
   });
 });
+
 var element = document.getElementById("top");
 var under = document.getElementById("under");
 const scroll =
@@ -66,23 +67,90 @@ $(function () {
 
 });
 var i = 0
-var hogehoge = function(){
- // 一度全ての要素を非表示にする
+var hogehoge = function () {
+  // 一度全ての要素を非表示にする
   $(".slide-img").hide()
   // 特定の要素を表示する
   $(`.slide-img:eq(${i})`).show()
   // 次に表示する要素を算出する
   // 最後の要素の場合は最初に戻る
-if (i == 3) {
+  if (i == 3) {
     document.getElementById("video").play()
   }
   i < $(".slide-img").length - 1 ? i++ : i = 0
 
-} 
+}
 hogehoge();
-setInterval(function() {
-hogehoge();
+setInterval(function () {
+  hogehoge();
 }, 7000)
+
+var
+  cursor = $(".fa-paw"),
+  follower = $(".follower"),
+  cWidth = 20, //カーソルの大きさ
+  fWidth = 40, //フォロワーの大きさ
+  delay = 10, //数字を大きくするとフォロワーがより遅れて来る
+  mouseX = 0, //マウスのX座標
+  mouseY = 0, //マウスのY座標
+  posX = 0, //フォロワーのX座標
+  posY = 0, //フォロワーのX座標
+  offset = 1000;
+//カーソルの遅延アニメーション
+//ほんの少ーーーしだけ遅延させる 0.001秒
+TweenMax.to({}, .001, {
+  repeat: -1,
+  onRepeat: function () {
+    posX += (mouseX - posX) / delay;
+    posY += (mouseY - posY) / delay;
+
+    TweenMax.set(follower, {
+      css: {
+        left: posX - (fWidth / 2),
+        top: posY - (fWidth / 2)
+      }
+    });
+
+    TweenMax.set(cursor, {
+      css: {
+        left: mouseX - (cWidth / 2),
+        top: mouseY - (cWidth / 2)
+      }
+    });
+  }
+});
+
+//マウス座標を取得
+$(document).on("mousemove", function (e) {
+  mouseX = e.pageX;
+  mouseY = e.pageY;
+});
+
+$(".paw-box").on({
+  "mouseenter": function () {
+    offset = 0;
+    cursor.addClass("is-active");
+    follower.addClass("is-active");
+  },
+  "mouseleave": function () {
+    offset = 1000;
+    cursor.removeClass("is-active");
+    follower.removeClass("is-active");
+  }
+});
+$(".btn-6").on({
+  "mouseenter": function () {
+    offset = 0;
+    cursor.addClass("paw-active2");
+    follower.addClass("paw-active");
+    
+  },
+  "mouseleave": function () {
+    offset = 1000;
+    cursor.removeClass("paw-active2");
+    follower.removeClass("paw-active");
+  }
+});
 
 $('.gray').css('visibility', 'hidden');
 $(window).scroll(function () {
@@ -95,6 +163,7 @@ $(window).scroll(function () {
     }
   });
 });
+
 $('.gray-right').css('visibility', 'hidden');
 $(window).scroll(function () {
   var windowHeight = $(window).height(),
@@ -193,3 +262,19 @@ $(window).scroll(function () {
   });
 });
 
+$(function() {  
+  $('.btn-6')
+    .on('mouseenter', function(e) {
+            var parentOffset = $(this).offset(),
+            relX = e.pageX - parentOffset.left,
+            relY = e.pageY - parentOffset.top;
+            $(this).find('span').css({top:relY, left:relX})
+    })
+    .on('mouseout', function(e) {
+            var parentOffset = $(this).offset(),
+            relX = e.pageX - parentOffset.left,
+            relY = e.pageY - parentOffset.top;
+        $(this).find('span').css({top:relY, left:relX})
+    });
+  // $('[href=#]').click(function(){return false});
+});
